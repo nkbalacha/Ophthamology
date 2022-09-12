449ef4667810a69eaa16340c87b670a63a6ec535
Nitin Balachandran <nitinkb2@illinois.edu>
2022-09-12 15:38:16 -0500

    removed docker files

--- dockerfile/compile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker build -t nitinkb/pytorch:vivaldi -f Dockerfile.cu11.1 .
-docker push nitinkb/pytorch:vivaldi
-
-# docker build -t neerajwagh/opencl:test -f Dockerfile.opencl .
-# docker push neerajwagh/opencl:test
--- dockerfile/login.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cid=`docker ps | grep nitinkb/pytorch:vivaldi | awk '{print $1}'`
-# cid=`docker ps | grep neerajwagh/opencl:test | awk '{print $1}'`
-
-docker exec -ti $cid /bin/bash
-
-
