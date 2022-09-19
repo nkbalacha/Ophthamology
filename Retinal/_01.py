64beadaef80ecb618a46220e6465fe421cf6bd56
Nitin Balachandran <nitinkb2@illinois.edu>
2022-09-19 15:08:14 -0500

    Updating Code for testing data

--- Retinal/_01.py
+++ Retinal/_01.py
@@ -5,8 +5,36 @@ import toml
 from glob2 import glob
 from tqdm import tqdm
 
+from models.denoising_diffusion_pytorch import Unet, GaussianDiffusion, Trainer
+
 def main():
-    print("Hello")
+    model = Unet(
+    dim = 64,
+    dim_mults = (1, 2, 4, 8)
+	).cuda()
+
+	diffusion = GaussianDiffusion(
+    	model,
+    	image_size = 1024,
+    	timesteps = 1000,           # number of steps
+    	sampling_timesteps = 250,   # number of sampling timesteps (using ddim for faster inference [see citation for ddim paper])
+    	loss_type = 'l1'            # L1 or L2
+	).cuda()
+
+	trainer = Trainer(
+    	diffusion,
+    	#'/home/nitin/data/resized_train_cropped/resized_train_cropped',
+		_KAGGLE_DATA_DIR,
+    	train_batch_size = 32,
+    	train_lr = 8e-5,
+    	train_num_steps = 700000,         # total training steps
+    	gradient_accumulate_every = 2,    # gradient accumulation steps
+    	ema_decay = 0.995,                # exponential moving average decay
+    	results_folder= _RESULTS_DIR,
+		amp = True                        # turn on mixed precision
+	)
+
+	trainer.train()
 
 if __name__ == '__main__':
 
